0a0bf6a7c7eada2c78b5aeb6bae07726632e0d29
CSharper63 <gitcommit.ci158@silomails.com>
2024-07-04 09:51:25 +0200

    add compute result of all 2^32 mask applied on all mic and frmpayload

--- analysis/test_bit_quality/src/main.rs
+++ analysis/test_bit_quality/src/main.rs
@@ -18,36 +18,111 @@ struct MicList {
     mic: Vec<i32>,
 }
 
-#[derive(Serialize, Debug)]
+#[derive(Serialize, Debug, Deserialize)]
 struct Issue {
     i: u32,
     odd_count: u32,
     even_count: u32,
 }
 
-fn find_threshold_odd_count(total_mics: u32, error_threshold: f64) -> u32 {
-    let binom = Binomial::new(0.5, total_mics.into()).unwrap();
-    for odd_count in (0..=total_mics).rev() {
+#[derive(Deserialize)]
+struct IssueList {
+    issues: Vec<Issue>,
+}
+
+#[derive(Serialize)]
+struct IssueHex {
+    i: String,
+    odd_count: u32,
+    even_count: u32,
+}
+
+#[derive(Serialize)]
+struct IssueListHex {
+    issues: Vec<IssueHex>,
+}
+
+#[derive(Serialize)]
+struct IHexList {
+    i_tab: Vec<String>,
+}
+
+#[derive(Serialize)]
+struct IBinList {
+    i_tab: Vec<String>,
+}
+
+#[derive(Deserialize)]
+struct FrmPayloadList {
+    frmpayload: Vec<String>,
+}
+
+fn find_threshold_odd_count(total_blocks: u32, error_threshold: f64) -> u32 {
+    let binom = Binomial::new(0.5, total_blocks.into()).unwrap();
+    for odd_count in (0..=total_blocks).rev() {
         let p_value = binom.cdf(odd_count as u64) as f64 * 2.0; // two sided
         if p_value <= error_threshold {
             return odd_count;
         }
     }
-    total_mics
+    total_blocks
 }
 
-fn test_bit_quality(mic_list: &[i32], verbose: bool) -> Vec<Issue> {
-    let total_mics = mic_list.len() as u32;
+fn truncate_frm_payload(frmpayload_list: &FrmPayloadList) -> Vec<i32> {
+    frmpayload_list
+        .frmpayload
+        .iter()
+        .filter_map(|hex_str| {
+            if let Ok(full_value) = u64::from_str_radix(hex_str, 16) {
+                let truncated_value = (full_value & 0xFFFFFFFF) as i32;
+                Some(truncated_value)
+            } else {
+                None
+            }
+        })
+        .collect()
+}
+
+fn read_json<T: for<'de> serde::Deserialize<'de>>(
+    path: &str,
+) -> Result<T, Box<dyn std::error::Error>> {
+    let file = File::open(path)?;
+    let reader = BufReader::new(file);
+    let data = serde_json::from_reader(reader)?;
+    Ok(data)
+}
+
+fn write_json(path: &str, data: &serde_json::Value) -> Result<(), Box<dyn std::error::Error>> {
+    let mut file = File::create(path)?;
+    serde_json::to_writer_pretty(&file, data)?;
+    Ok(())
+}
+
+fn convert_issues_to_hex(issue_list: IssueList) -> IssueListHex {
+    let issues_hex: Vec<IssueHex> = issue_list
+        .issues
+        .into_iter()
+        .map(|issue| IssueHex {
+            i: format!("{:X}", issue.i),
+            odd_count: issue.odd_count,
+            even_count: issue.even_count,
+        })
+        .collect();
+    IssueListHex { issues: issues_hex }
+}
+
+fn test_bit_quality(list_blocks: &[i32]) -> Vec<Issue> {
+    let total_blocks = list_blocks.len() as u32;
     let error_threshold = 0.0000001;
-    let threshold_odd_count = find_threshold_odd_count(total_mics, error_threshold);
-    let expected_even = total_mics - threshold_odd_count;
+    let threshold_odd_count = find_threshold_odd_count(total_blocks, error_threshold);
+    let expected_even = total_blocks - threshold_odd_count;
 
     let issues = Arc::new(Mutex::new(Vec::new()));
     let chunk_size = u32::MAX / 10000;
     let counter = Arc::new(AtomicUsize::new(0));
     let in_percent = error_threshold as f64 * 100.0;
 
-    println!("Count of MIC: {:?}", total_mics);
+    println!("Count of blocks: {:?}", total_blocks);
     println!(
         "Odd count threasold for {:?}% : {:?}",
         in_percent, threshold_odd_count
@@ -60,7 +135,7 @@ fn test_bit_quality(mic_list: &[i32], verbose: bool) -> Vec<Issue> {
         }
 
         let mut odd_count = 0;
-        for &mic in mic_list {
+        for &mic in list_blocks {
             let mic = mic as u32; // convert all signed mic
             let and_result = i & mic;
             let bit_count = and_result.count_ones();
@@ -74,7 +149,7 @@ fn test_bit_quality(mic_list: &[i32], verbose: bool) -> Vec<Issue> {
             issues.push(Issue {
                 i,
                 odd_count,
-                even_count: total_mics - odd_count,
+                even_count: total_blocks - odd_count,
             });
         }
     });
@@ -83,16 +158,33 @@ fn test_bit_quality(mic_list: &[i32], verbose: bool) -> Vec<Issue> {
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let file = File::open("../all_mic.json")?;
-    let reader = BufReader::new(file);
-    let mic_list: MicList = serde_json::from_reader(reader)?;
+    // process mic list
+    /* let mic_list: MicList = read_json("../all_frmpayload.json")?;
     let start_time = Utc::now();
-    println!("Start time: {}", start_time);
-    let issues = test_bit_quality(&mic_list.mic, false);
+    let issues = test_bit_quality(&mic_list.mic);
     let end_time = Utc::now();
+    println!("Start time: {}", start_time);
     println!("End time: {}", end_time);
-    let issue_json = json!({ "issues": issues });
-    let mut issue_file = File::create("../issue.json")?;
-    serde_json::to_writer_pretty(&issue_file, &issue_json)?;
+    write_json("../issue_mic.json", &json!({ "issues": issues }))?; */
+
+    // process frm payload
+    /* let frmpayload_list = read_json("../all_frmpayload.json")?;
+    let truncated_mic_list = truncate_frm_payload(&frmpayload_list);
+
+    let start_time = Utc::now();
+    let issues = test_bit_quality(&truncated_mic_list);
+    let end_time = Utc::now();
+    println!("Start time: {}", start_time);
+    println!("End time: {}", end_time);
+    write_json("../issue_frmpayload.json", &json!({ "issues": issues }))?; */
+
+    // convert i to hex:
+    let issue_list: IssueList = read_json("../issue_frmpayload.json")?;
+    let issues_hex = convert_issues_to_hex(issue_list);
+    write_json(
+        "output_frmpayload_hex.json",
+        &json!({ "issue_frmpayload.json": issues_hex }),
+    )?;
+
     Ok(())
 }
